a443e49c96b75b7a01ce81e1f6bdcad58d5fb744
Prasantacharya <acharp@rpi.edu>
2021-04-15 21:37:49 -0400

    finished the lab

--- checkpoint5.py
+++ checkpoint5.py
@@ -1,14 +1,19 @@
 from pymongo import MongoClient
+import random
+import datetime
 client = MongoClient()
 
 
-def random_word_requester():
+def random_word_requester(wordList):
     '''
     This function should return a random word and its definition and also
     log in the MongoDB database the timestamp that it was accessed.
     '''
-    return
+    randWord = random.choice(list(wordList.find()))
+    print(randWord)
+    wordList.update_one({"word": randWord}, {"$push": {"dates": datetime.datetime.utcnow().isoformat()}})
+    return wordList.find_one({"word": randWord})
 
 
 if __name__ == '__main__':
-    print(random_word_requester())
+    print(random_word_requester(client["mongo_db_lab"]["definitions"]))
